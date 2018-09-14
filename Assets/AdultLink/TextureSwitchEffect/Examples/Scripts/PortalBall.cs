909b27cb186123a07e5ae3452b5cdd0e5db1101d
Willy Angel <gguillermix@gmail.com>
2018-09-14 18:57:45 +0200

    Restructuring

--- Assets/AdultLink/TextureSwitchEffect/Examples/Scripts/PortalBall.cs
+++ Assets/AdultLink/TextureSwitchEffect/Examples/Scripts/PortalBall.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class asdasd : MonoBehaviour {
+public class PortalBall : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
