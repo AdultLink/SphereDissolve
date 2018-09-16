23d9e1c63eb24284e2fbf0e484485ee16459ca3a
Willy Angel <gguillermix@gmail.com>
2018-09-16 19:28:30 +0200

    More flexible rotateAround script

--- Assets/AdultLink/TextureSwitchEffect/Examples/Scripts/RotateAround.cs
+++ Assets/AdultLink/TextureSwitchEffect/Examples/Scripts/RotateAround.cs
@@ -5,8 +5,9 @@ using UnityEngine;
 public class RotateAround : MonoBehaviour {
 	public Transform pivotPoint;
 	public float rotationSpeed;
+	public Vector3 axis;
 	// Update is called once per frame
 	void FixedUpdate () {
-		transform.RotateAround(pivotPoint.transform.position, Vector3.up, rotationSpeed);
+		transform.RotateAround(pivotPoint.transform.position, axis, rotationSpeed);
 	}
 }
